ae53ccb631d49e6c21c0249b7d1ff212c583c06d
Mena <dayjacs03@gmail.com>
2025-12-29 15:53:04 -0600

    working onn login-sign up / home

--- middleware/validators.js
+++ middleware/validators.js
@@ -1,1 +1,10 @@
 const { body, validationResult } = require("express-validator");
+
+[body("fName").trim().notEmpty().withMessage("name cannot be empty")][
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("username cannot be empty")
+    .isAlpha()
+    .withMessage("username can only contian letters + numbers")
+][body("email")][body("password")];

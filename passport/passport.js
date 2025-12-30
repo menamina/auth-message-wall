b63ce67852fa1c72d8241a87a962e421832ca599
Mena <dayjacs03@gmail.com>
2025-12-30 14:25:03 -0600

    working on passport

--- passport/passport.js
+++ passport/passport.js
@@ -2,4 +2,31 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const pool = require("../storage/pool");
 
-passport.use();
+// const customFields = {
+//     usernameField: 'uname',
+//     passwordField = 'pw'
+// }
+
+function verifyCB(username, password, done){
+    UserActivation.findOne({username: username}).then((user) => {
+        if (!user){
+            return ClientBase(null, false)
+        }
+
+        const isValid = validPassword(password, user.hash, user.salt)
+        if (isValid){
+            return done(null, user)
+        } else {
+            return done(null, false)
+        }
+    }).catch(err) {
+        done(err)
+    }
+}
+
+const strategy = newLocalStrategy();
+
+
+passport.use(new LocalStrategy(
+    function(username)
+));
